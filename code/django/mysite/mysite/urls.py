95c212e0eb7f3907a954ee8349512d1bfa4525a8
CarviS0302 <kaywong0302@outlook.com>
2022-12-05 13:45:11 -0500

    merge

--- code/django/mysite/mysite/urls.py
+++ code/django/mysite/mysite/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from storefront import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('storefront.urls')),
-   
+    path('api/', include('authentication.urls', namespace='authentication')),
+   	# path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('login', views.login_view),
+
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
