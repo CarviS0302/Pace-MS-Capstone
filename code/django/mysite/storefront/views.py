e776834ad14979d2ea9ceb027e85c3566a8e2c4e
Peter Torrente <pvtorrente@gmail.com>
2022-12-11 13:02:10 -0500

    updated order.js

--- code/django/mysite/storefront/views.py
+++ code/django/mysite/storefront/views.py
@@ -10,11 +10,46 @@ from .serializer import *
 # Create your views here.
 
 class ItemView(APIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     def get(self, request: Request):
         items = Item.objects.all()
         serialized_item = ItemSerializer(items, many = True)
         return Response(serialized_item.data)
+
+class FindItemView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request: Request, pk):
+        items = Item.objects.get(pk = pk)
+        serialized_item = ItemSerializer(items, many = False)
+        return Response(serialized_item.data)
+
+class AddItemView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request: Request):
+        item = ItemSerializer(data = request.data)
+        if item.is_valid():
+            item.save()
+            return Response(item.data)
+        else:
+            return Response(item.errors)
+
+class UpdateItemView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def put(self, request: Request, pk):
+        item = Item.objects.get(pk = pk)
+        item_serializer = ItemSerializer(item, data = request.data)
+        if item_serializer.is_valid():
+            item_serializer.save()
+            return Response(item_serializer.data)
+        else:
+            return Response(item_serializer.errors)
+
+class DeleteItemView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def delete(self, request: Request, pk):
+        item = Item.objects.get(pk = pk)
+        item.delete()
+        return Response("Item deleted")
 
 class SupplierView(APIView):
     permission_classes = (IsAuthenticated,)
@@ -29,7 +64,7 @@ class FindSupplierView(APIView):
         suppliers = Supplier.objects.get(pk = pk)
         serialized_supplier = SupplierSerializer(suppliers, many = False)
         return Response(serialized_supplier.data)
-    
+
 class AddSupplierView(APIView):
     permission_classes = (IsAuthenticated,)
     def post(self, request: Request):
@@ -39,7 +74,7 @@ class AddSupplierView(APIView):
             return Response(supplier.data)
         else:
             return Response(supplier.errors)
-        
+
 class UpdateSupplierView(APIView):
     permission_classes = (IsAuthenticated,)
     def put(self, request: Request, pk):
@@ -50,15 +85,15 @@ class UpdateSupplierView(APIView):
             return Response(supplier_serializer.data)
         else:
             return Response(supplier_serializer.errors)
-        
+
 class DeleteSupplierView(APIView):
     permission_classes = (IsAuthenticated,)
     def delete(self, request: Request, pk):
         supplier = Supplier.objects.get(pk = pk)
         supplier.delete()
         return Response("Supplier deleted")
-    
-  
+
+
 class CustomerView(APIView):
     def get(self, request: Request):
         customers = Customer.objects.all()
@@ -89,7 +124,37 @@ class OrderView(APIView):
         serialized_order = OrderSerializer(orders, many = True)
         return Response(serialized_order.data)
 
-    
+class FindOrderView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request: Request, pk):
+        orders = Order.objects.get(pk = pk)
+        serialized_order = OrderSerializer(orders, many = False)
+        return Response(serialized_order.data)
 
+class AddOrderView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request: Request):
+        order = OrderSerializer(data = request.data)
+        if order.is_valid():
+            order.save()
+            return Response(order.data)
+        else:
+            return Response(order.errors)
 
+class UpdateOrderView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def put(self, request: Request, pk):
+        order = Order.objects.get(pk = pk)
+        order_serializer = OrderSerializer(order, data = request.data)
+        if order_serializer.is_valid():
+            order_serializer.save()
+            return Response(order_serializer.data)
+        else:
+            return Response(order_serializer.errors)
 
+class DeleteOrderView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def delete(self, request: Request, pk):
+        order = Order.objects.get(pk = pk)
+        order.delete()
+        return Response("Order deleted")
--- frontend/src/pages/order.js
+++ frontend/src/pages/order.js
@@ -1,38 +1,164 @@
 import React from "react";
 
-export default function order() {
+export default function Order() {
+
   const [orders, setOrders] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+  function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+  return JSON.parse(tokenString);
+
+}
+  let formDatas = {
+    orderID: "",
+    itemID: "",
+    quantity: "",
+    orderDate: "",
+    orderPrice: "",
+    customerID: "",
+    shipID: "",
+  };
   React.useEffect(() => {
-    fetch("http://localhost:8000/order")
+    fetch("http://localhost:8000/order",{
+      headers: new Headers({
+        'Authorization': 'Bearer '+ getToken()
+    })
+    })
       .then((res) => res.json())
       .then((data) => {
         setOrders(data);
         orders.map((order) => {});
         setLoading(false);
+      }).catch((error) => {
+        console.log('error: ' + error);
+        // this.setState({ requestFailed: true });
       });
   }, []);
-
-  const editOrders = (orderID) => {
-    // console.log(orderID);
+  const inputChanged = (event) => {
+    formDatas[event.target.name] = event.target.value;
   };
-  const deleteOrder = (orderID) => {
+  function resetForm() {
+    document.getElementById("orderForm").reset();
+    formDatas = {
+      orderID: "",
+      itemID: "",
+      quantity: "",
+      orderDate: "",
+      orderPrice: "",
+      customerID: "",
+      shipID: "",
+    };
+  }
+  const handleSubmit = (event) => {
+    submitForm(formDatas);
+  };
+  const editOrder = (orderID) => {
+    console.log(orderID);
+    console.log("edit");
+    fetch("http://localhost:8000/order/find/" + orderID,{
+
+        headers: new Headers({
+          'Authorization': 'Bearer '+ getToken()
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        formDatas.orderID = data.orderID;
+        formDatas.itemID = data.itemID;
+        formDatas.quantity = data.quantity;
+        formDatas.orderDate = data.orderDate;
+        formDatas.orderPrice = data.orderPrice;
+        formDatas.customerID = data.customerID;
+        formDatas.shipID = data.shipID;
+        console.log(formDatas);
+        addOrder();
+      });
+
+  };
+  function deleteOrder(orderID) {
     let confirmDelete = window.confirm("Are you sure you want to delete?");
-    // if (confirmDelete) {
-    //   fetch("http://localhost:8000/order/" + orderID, {
-    //     method: "DELETE",
-    //   }).then((res) => {
-    //     if (res.ok) {
-    //       alert("Order deleted");
-    //       window.location.reload();
-    //     }
-    //   });
-    // }
+    if (confirmDelete) {
+      fetch("http://localhost:8000/order/delete/" + orderID, {
+        method: "DELETE",
+        headers: new Headers({
+          'Authorization': 'Bearer '+ getToken()
+      })
+      }).then((res) => {
+        if (res.ok) {
+          alert("Order deleted");
+          window.location.reload();
+        }
+      });
+    }
 
   };
+  const saveChanges = () => {
+    // get the values from the form
+    console.log("saving changes");
+    submitForm(formDatas);
+    // reload the page
+    closeModal();
+    // window.location.reload();
+
+  };
+  function getOrder(orderID) {
+    fetch("http://localhost:8000/order/find/" + orderID)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      }
+      );
+  };
+
+  function submitForm(datas) {
+    if (datas.orderID === "") {
+      fetch("http://localhost:8000/order/add/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          'Authorization': 'Bearer '+ getToken(),
+        },
+        body: JSON.stringify(datas),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          resetForm();
+          window.location.reload();
+        });
+    } else {
+    fetch("http://localhost:8000/order/update/" + datas.orderID+"/", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer '+ getToken(),
+      },
+      body: JSON.stringify(datas),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resetForm();
+        window.location.reload();
+      // });
+    });
+  }
+
+  }
+
   const addOrder = () => {
     let modal = document.getElementById("myModal");
     modal.style.display = "block";
+    // set the values of the form
+    for(let key in formDatas) {
+      try {
+      document.getElementById(key).value = formDatas[key];
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
   };
   const closeModal = () => {
     let modal = document.getElementById("myModal");
@@ -43,8 +169,6 @@ export default function order() {
     return <div>Loading...</div>;
   }
 
-
-
     return (
   <div>
     <div>
@@ -53,15 +177,13 @@ export default function order() {
       <button className="button is-primary" onClick={addOrder}>Add Order</button>
     </div>
 
-
-
       <div className="data-table">
       <table className="my-table">
       <thead>
         <tr>
+          <th>#</th>
           <th>Item ID</th>
           <th>Quantity</th>
-          <th>Email</th>
           <th>Order Date</th>
           <th>Order Price</th>
           <th>Customer ID</th>
@@ -70,6 +192,7 @@ export default function order() {
         </tr>
       </thead>
       <tbody>
+
         {orders.map((order, i) => (
           <tr key={i}>
             <td>{i+1}</td>
@@ -79,12 +202,9 @@ export default function order() {
             <td>{order.orderPrice}</td>
             <td>{order.customerID}</td>
             <td>{order.shipID}</td>
-            <td>{order.state}</td>
-            <td>{order.zip}</td>
-            <td>{order.note}</td>
             <td>
-              <button className="button is-warning is-fullwidth" onClick={editOrder(order.orderID)}>Edit</button>
-              <button className="button is-danger is-fullwidth" onClick={deleteOrder}>Delete</button>
+              <button className="button is-warning is-fullwidth" onClick={() => editOrder(order.orderID)}>Edit</button>
+              <button className="button is-danger is-fullwidth" onClick={() => deleteOrder(order.orderID)}>Delete</button>
             </td>
           </tr>
         ))}
@@ -96,56 +216,50 @@ export default function order() {
     <div className="modal-content">
       <header className="modal-header">
         <p className="modal-card-title">Add Order</p>
-        <button className="button delete is-danger" aria-label="close" onClick={closeModal} >X</button>
+        <button className="button delete is-danger" aria-label="close" onClick={closeModal}>X</button>
       </header>
       <section className="modal-body">
-        <form>
+        <form id="orderForm">
           <div className="field">
             <label className="label">Item ID</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Item ID" />
+              <input className="input" type="text"  name="itemID" id="itemID" onChange={inputChanged} placeholder="Item ID" />
             </div>
             </div>
             <div className="field">
             <label className="label">Quantity</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Quantity" />
-              </div>
-              </div>
-              <div className="field">
-            <label className="label">Email</label>
-            <div className="control">
-              <input className="input" type="tel" placeholder="Email" />
+              <input className="input" type="text" name="quantity" id="quantity"  onChange={inputChanged} placeholder="Quantity" />
               </div>
               </div>
               <div className="field">
             <label className="label">Order Date</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Order Date" />
+              <input className="input" type="tel" name="orderDate" id="orderDate"  onChange={inputChanged} placeholder="Order Date" />
               </div>
               </div>
               <div className="field">
             <label className="label">Order Price</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Order Price" />
+              <input className="input" type="text" name="orderPrice" id="orderPrice"  onChange={inputChanged} placeholder="Order Price" />
               </div>
               </div>
               <div className="field">
             <label className="label">Customer ID</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Customer ID" />
+              <input className="input" type="text" name="customerID" id="customerID"  onChange={inputChanged} placeholder="Customer ID" />
               </div>
               </div>
               <div className="field">
             <label className="label">Ship ID</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Ship ID" />
+              <input className="input" type="text" name="shipID" id="shipID"  onChange={inputChanged} placeholder="Ship ID" />
               </div>
               </div>
         </form>
       </section>
       <footer className="modal-footer">
-        <button className="button is-success">Save changes</button>
+        <button className="button is-success" onClick={saveChanges}>Save changes</button>
         <button className="button is-danger" onClick={closeModal}>Cancel</button>
       </footer>
 
